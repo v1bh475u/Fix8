69678be58bbc7dfd1446e17937a88531ca291efa
Praneeth Sarode <praneethsarode@gmail.com>
2024-11-09 20:55:22 +0530

    add: Assembly programs and their assembled binaries
    Signed-off-by: Praneeth Sarode <praneethsarode@gmail.com>

---
+++ asm/arithmetic.asm
@@ -0,0 +1,21 @@
+; This test shows the use of simple arithmetic operations.
+
+MOVI $2
+MOV R1, R0
+MOVI $3
+ADD R1, R0 ; Addition
+
+MOVI $D
+SUB R1, R0 ; Subtraction
+
+MOVI $8
+SHL R1, R2
+MOVI $C
+MUL R1, R0 ; Multiplication
+
+MOV R3, R0
+MOVI $4
+MOV R1, R0
+DIV R3, R1 ; Division
+
+HLT
---
+++ asm/logical.asm
@@ -0,0 +1,26 @@
+; This test shows the utility of NAND to compute all possible bitwise operations.
+
+MOVI $4
+MOV R1, R0
+MOVI $F
+NAND R1, R0 ; This is and
+
+NAND R1, R1
+MOVI $9
+NAND R0, R0 ; This is or
+
+NAND R1, R1
+NAND R1, R0
+MOVI $8
+SHL R1, R0
+MOVI $D
+MOV R3, R0
+MOV R2, R1
+NAND R0, R0
+NAND R2, R0
+NAND R1, R1
+NAND R3, R1
+NAND R3, R2 ; This is xor
+MOV R0, R3
+
+HLT
