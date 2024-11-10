3f6745eae5583a4869eabedd4379b98989564d72
5h3rl0ckH0lm3s <kritik1026@gmail.com>
2024-11-10 07:12:29 +0000

    change: modified after changing ISA

--- asm/arithmetic.asm
+++ asm/arithmetic.asm
@@ -1,20 +1,18 @@
 ; This test shows the use of simple arithmetic operations.
 
-MOVI $2
+MOVL $2
 MOV R1, R0
-MOVI $3
+MOVL $3
 ADD R1, R0 ; Addition
 
-MOVI $D
+MOVL $D
 SUB R1, R0 ; Subtraction
 
-MOVI $8
-SHL R1, R2
-MOVI $C
+MOVL $5
 MUL R1, R0 ; Multiplication
 
 MOV R3, R0
-MOVI $4
+MOVL $4
 MOV R1, R0
 DIV R3, R1 ; Division
 
