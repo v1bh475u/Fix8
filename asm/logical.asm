1108541c6096bc1cc554600246e173232d724159
5h3rl0ckH0lm3s <kritik1026@gmail.com>
2024-11-10 10:09:43 +0000

    change: corrected the file

--- asm/logical.asm
+++ asm/logical.asm
@@ -1,26 +1,30 @@
 ; This test shows the utility of NAND to compute all possible bitwise operations.
 
-MOVI $4
+MOVL $5
+NAND R0, R0 ; R0 contains NOT($5) finally
+
+MOVL $4
 MOV R1, R0
-MOVI $F
-NAND R1, R0 ; This is and
+MOVL $F
+NAND R1, R0 
+NAND R1, R1 ; R1 contains $4 AND $F finally
 
-NAND R1, R1
-MOVI $9
-NAND R0, R0 ; This is or
-
-NAND R1, R1
-NAND R1, R0
-MOVI $8
-SHL R1, R0
-MOVI $D
-MOV R3, R0
-MOV R2, R1
+MOVL $3
+MOV R1, R0
+MOVL $9
 NAND R0, R0
-NAND R2, R0
 NAND R1, R1
-NAND R3, R1
-NAND R3, R2 ; This is xor
-MOV R0, R3
+NAND R1, R0 ; R1 contains $3 OR $9 finally
+
+MOVL $8
+MOV R1, R0
+MOVL $D
+MOV R2, R0
+MOV R3, R1
+NAND R2, R2
+NAND R3, R3
+NAND R3, R0
+NAND R2, R1
+NAND R3, R2 ; R3 contains $8 XOR $D finally
 
 HLT
