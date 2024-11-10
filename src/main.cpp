db3668e5cf67abadfff313f1a8937a83299316ed
Praneeth Sarode <praneethsarode@gmail.com>
2024-11-10 14:27:52 +0530

    chore: update main.cpp
    Signed-off-by: Praneeth Sarode <praneethsarode@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,10 @@
 
 #include "../include/assembler.hpp"
 #include "../include/emulator.hpp"
+#include "../include/colors.hpp"
 
 using namespace std;
 namespace fs = std::filesystem;
-
-// ANSI color codes
-const string RESET = "\033[0m";
-const string RED = "\033[31m";
-const string GREEN = "\033[32m";
-const string YELLOW = "\033[33m";
-const string BLUE = "\033[34m";
-const string CYAN = "\033[36m";
 
 int assemble_file(string file_name, string output_file_name)
 {
